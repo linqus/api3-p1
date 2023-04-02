19f4d5c035fe76a6d5425abec855a39c8d088860
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-02 07:57:31 +0200

    plunderedAgo my ver

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -10,6 +10,7 @@ use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
 use App\Repository\DragonTreasureRepository;
+use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -114,6 +115,13 @@ class DragonTreasure
     public function getPlunderedAt(): ?\DateTimeImmutable
     {
         return $this->plunderedAt;
+    }
+
+    public function getPlunderedAgo(): ?string
+    {
+        $agoCarbon = new Carbon($this->plunderedAt);
+        $agoText = $agoCarbon->ago();
+        return $agoText;
     }
 
     public function getIsPublished(): ?bool
