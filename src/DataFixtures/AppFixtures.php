e1b432387164d5a963f18414cb8f1901842e45ec
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-04 13:09:34 +0200

    groups, validation, UserFactory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\DragonTreasureFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,5 +13,6 @@ class AppFixtures extends Fixture
     {
 
         DragonTreasureFactory::createMany(40);
+        UserFactory::createMany(10);
     }
 }
