d9f083c6f22f533cce97328b34921a3deb98e5bf
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-04 22:54:43 +0200

    install symfony/ux-react

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
